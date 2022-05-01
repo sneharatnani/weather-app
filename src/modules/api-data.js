5348de18219671b549d341446929816ccce61f51
sneharatnani <sneharatnani5@gmail.com>
2022-05-01 12:17:23 +0530

    change the protocol

--- src/modules/api-data.js
+++ src/modules/api-data.js
@@ -3,7 +3,7 @@ import { showError, getDayName, changeBackground } from "./utility.js";
 async function getData(city) {
   try {
     let response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9814c6dc9ff4e95b4f19390d2a44256d`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9814c6dc9ff4e95b4f19390d2a44256d`,
       { mode: "cors" }
     );
     // check for error from response
